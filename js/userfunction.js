d7977a4ab0dbd51e3c37a1a6db277433681b0d61
홍길동 <50073413+pam7461@users.noreply.github.com>
2020-04-20 15:25:28 +0900

    Add files via upload

---
+++ js/userfunction.js
@@ -0,0 +1,16 @@
+function h1Color(){
+  var mainTit=document.querySelector('h1');
+  mainTit.style.color='green';
+}
+function h1Size(){
+  var mainTit=document.querySelector('h1');
+  mainTit.style.fontSize='60px';
+}
+function h1Hide(){
+  var mainTit=document.querySelector('h1');
+  mainTit.style.visibility="hidden"
+}
+function h1Show(){
+  var mainTit=document.querySelector('h1');
+  mainTit.style.visibility='visible';
+}
